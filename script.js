4cb239a0c29649bc1eb4cf8e95022bc6ac3ee094
anacglopes <110110002+anacglopes@users.noreply.github.com>
2024-08-05 11:17:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('start-mapping').addEventListener('click', function() {
+    alert('Início do mapeamento com drones!');
+    // Aqui você pode adicionar código para iniciar o mapeamento, como interagir com APIs ou carregar um mapa.
+});
